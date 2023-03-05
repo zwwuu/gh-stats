8a906e16c6c06eb94acfd6ee21e70579b6f0e45a
zwwuu <98434813+zwwuu@users.noreply.github.com>
2023-03-05 01:59:33 -0500

    track web vitals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,16 @@
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import Layout from "@/components/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
 import SEO from "@/lib/seo";
 import Script from "next/script";
+import { event, GoogleAnalytics } from "nextjs-google-analytics";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo {...SEO} />
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script
-        dangerouslySetInnerHTML={{
-          __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-  
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}');`,
-        }}
-        id="google-analytics"
-        strategy="afterInteractive"
-      />
+      <GoogleAnalytics trackPageViews />
       <Script
         src={`//z-na.amazon-adsystem.com/widgets/onejs?MarketPlace=US&adInstanceId=${process.env.NEXT_PUBLIC_AMAZON_AD_ID}`}
         async
@@ -36,4 +22,13 @@ export default function App({ Component, pageProps }: AppProps) {
       </ChakraProvider>
     </>
   );
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  event(metric.name, {
+    event_category: metric.label === "web-vital" ? "Web Vitals" : "custom metric",
+    value: Math.round(metric.name === "CLS" ? metric.value * 1000 : metric.value), // values must be integers
+    event_label: metric.id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
 }
